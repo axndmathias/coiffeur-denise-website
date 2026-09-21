f076029b03db59a062710cb73679f64ae1bcd1b6
axnd <mathias.alexandre@gmail.com>
2026-09-21 19:30:57 +0200

    Use foto_89 for on-location bridal styling card

--- frontend/src/data/content.js
+++ frontend/src/data/content.js
@@ -49,7 +49,7 @@ export const BRIDE_EXPERIENCE = [
       "https://wa.me/41798666707?text=Hallo%20Denise%2C%20ich%20m%C3%B6chte%20einen%20Probetermin%20anfragen",
   },
   {
-    image: `${process.env.PUBLIC_URL}/foto_39.jpg`,
+    image: `${process.env.PUBLIC_URL}/foto_89.jpg`,
     alt: "Denise beim Styling einer Braut am Hochzeitstag",
     href:
       "https://wa.me/41798666707?text=Hallo%20Denise%2C%20ich%20m%C3%B6chte%20die%20Verf%C3%BCgbarkeit%20f%C3%BCr%20meinen%20Hochzeitstag%20anfragen",
